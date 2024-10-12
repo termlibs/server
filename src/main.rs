ae2795f68f0f4644d4f3fa19663f374f66879a76
Adam Huganir <adam@huganir.com>
2024-10-12 11:30:51 -0400

    wip linting

--- src/main.rs
+++ src/main.rs
@@ -7,102 +7,90 @@ mod static_site;
 mod types;
 
 use crate::types::ScriptResponse;
-use comrak::Options;
 use log::{error, info};
-use rocket::form::validate::Len;
-use rocket::futures::io::Cursor;
-use rocket::http::hyper::header::CONTENT_DISPOSITION;
-use rocket::http::uri::fmt::Kind::Path;
 use rocket::http::ContentType;
 use rocket::response::{content, Responder};
-use rocket::serde::Deserialize;
-use rocket::serde::__private::de::Content;
 use rocket::{http, response, Request, Response};
-use std::convert::Infallible;
-use std::env::join_paths;
-use std::net::SocketAddr;
-use std::ops::Deref;
 use std::path::PathBuf;
 use std::sync::LazyLock;
-use std::{env, error, path};
-use tokio::net::TcpListener;
+use std::env;
 use types::InstallQueryOptions;
 
 const FAVICON: &[u8] = include_bytes!("../favicon.ico");
 const TERMLIBS_ROOT: LazyLock<PathBuf> =
-    LazyLock::new(|| PathBuf::from(env::var("TERMLIBS_ROOT").unwrap_or("../".into())));
+  LazyLock::new(|| PathBuf::from(env::var("TERMLIBS_ROOT").unwrap_or("../".into())));
 
 fn setup_logger(log_level: &str) -> Result<(), fern::InitError> {
-    let log_level = log_level.to_uppercase();
-    let level = match log_level.as_str() {
-        "DEBUG" => log::LevelFilter::Debug,
-        "INFO" => log::LevelFilter::Info,
-        "WARN" => log::LevelFilter::Warn,
-        "ERROR" => log::LevelFilter::Error,
-        _ => log::LevelFilter::Info,
-    };
-    let _ = fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{} {}: {}",
-                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
-                record.level(),
-                message
-            ));
-        })
-        .level(level)
-        .chain(std::io::stdout())
-        .apply()?;
-    Ok(())
+  let log_level = log_level.to_uppercase();
+  let level = match log_level.as_str() {
+    "DEBUG" => log::LevelFilter::Debug,
+    "INFO" => log::LevelFilter::Info,
+    "WARN" => log::LevelFilter::Warn,
+    "ERROR" => log::LevelFilter::Error,
+    _ => log::LevelFilter::Info,
+  };
+  let _ = fern::Dispatch::new()
+    .format(|out, message, record| {
+      out.finish(format_args!(
+        "{} {}: {}",
+        chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+        record.level(),
+        message
+      ));
+    })
+    .level(level)
+    .chain(std::io::stdout())
+    .apply()?;
+  Ok(())
 }
 
 #[catch(404)]
 fn not_found(_req: &Request) -> content::RawHtml<String> {
-    let html = static_site::load_static("404.html").unwrap_or("".to_string());
-    let response = content::RawHtml(html);
-    response
+  let html = static_site::load_static("404.html").unwrap_or("".to_string());
+  let response = content::RawHtml(html);
+  response
 }
 
 #[get("/favicon.ico")]
 async fn favicon<'r>() -> (ContentType, Vec<u8>) {
-    (ContentType::Icon, FAVICON.to_vec())
+  (ContentType::Icon, FAVICON.to_vec())
 }
 
 #[get("/install/<app>?<q..>", rank = 1)]
 async fn install_handler(app: &str, mut q: InstallQueryOptions) -> ScriptResponse {
-    info!("{:?} {:?}", app, q);
-    q.set_app(app.to_owned());
-    let output = shell_files::create_install_script(Some(q)).await.unwrap();
-    let r = ScriptResponse::new(format!("install-{}.sh", app), output);
-    r
+  info!("{:?} {:?}", app, q);
+  q.set_app(app.to_owned());
+  let output = shell_files::create_install_script(Some(q)).await.unwrap();
+  let r = ScriptResponse::new(format!("install-{}.sh", app), output);
+  r
 }
 
 #[get("/", rank = 10)]
 async fn root_handler() -> content::RawHtml<String> {
-    info!("{:?}", "root");
-    let html = static_site::load_static("index.html").unwrap_or("".to_string());
-    let response = content::RawHtml(html);
-    response
+  info!("{:?}", "root");
+  let html = static_site::load_static("index.html").unwrap_or("".to_string());
+  let response = content::RawHtml(html);
+  response
 }
 
 #[launch]
 async fn rocket() -> _ {
-    let port = env::var("PORT")
-        .unwrap_or("8080".to_string())
-        .parse::<u16>()
-        .unwrap();
-    let log_level = env::var("LOG_LEVEL").unwrap_or("DEBUG".to_string());
-    let listen_ip: String = env::var("LISTEN").unwrap_or("0.0.0.0".to_string());
+  let port = env::var("PORT")
+    .unwrap_or("8080".to_string())
+    .parse::<u16>()
+    .unwrap();
+  let log_level = env::var("LOG_LEVEL").unwrap_or("DEBUG".to_string());
+  let listen_ip: String = env::var("LISTEN").unwrap_or("0.0.0.0".to_string());
 
-    let _ = setup_logger(log_level.as_str()).unwrap_or(());
+  let _ = setup_logger(log_level.as_str()).unwrap_or(());
 
-    info!("starting server at {:?}:{}", listen_ip, port);
+  info!("starting server at {:?}:{}", listen_ip, port);
 
-    let figment = rocket::Config::figment()
-        .merge(("port", port))
-        .merge(("address", listen_ip))
-        .merge(("ident", "termlibs".to_string()));
-    rocket::custom(figment)
-        .register("/", catchers![not_found])
-        .mount("/", routes![favicon, install_handler, root_handler])
+  let figment = rocket::Config::figment()
+    .merge(("port", port))
+    .merge(("address", listen_ip))
+    .merge(("ident", "termlibs".to_string()));
+  rocket::custom(figment)
+    .register("/", catchers![not_found])
+    .mount("/", routes![favicon, install_handler, root_handler])
 }
--- src/types.rs
+++ src/types.rs
@@ -5,84 +5,85 @@ use rocket::{http, response, Request, Response};
 use std::io::Cursor;
 
 pub(crate) trait QueryOptions {
-    fn to_args(&self) -> String;
+  fn to_args(&self) -> String;
 }
 
 #[derive(Debug, PartialEq, FromForm)]
 pub struct InstallQueryOptions {
-    version: Option<String>,
-    prefix: Option<String>,
-    app: Option<String>,
+  version: Option<String>,
+  prefix: Option<String>,
+  app: Option<String>,
 }
 
 impl QueryOptions for InstallQueryOptions {
-    fn to_args(&self) -> String {
-        format!(
-            "--version=\"{}\" --prefix=\"{}\" \"{}\"",
-            self.version(),
-            self.prefix(),
-            self.app()
-        )
-    }
+  fn to_args(&self) -> String {
+    format!(
+      "--version=\"{}\" --prefix=\"{}\" \"{}\"",
+      self.version(),
+      self.prefix(),
+      self.app()
+    )
+  }
 }
 
 impl InstallQueryOptions {
-    pub(crate) fn version(&self) -> String {
-        self.version.clone().unwrap_or("latest".to_string())
-    }
+  pub(crate) fn version(&self) -> String {
+    self.version.clone().unwrap_or("latest".to_string())
+  }
 
-    pub(crate) fn prefix(&self) -> String {
-        self.prefix.clone().unwrap_or("$HOME/.local".to_string())
-    }
+  pub(crate) fn prefix(&self) -> String {
+    self.prefix.clone().unwrap_or("$HOME/.local".to_string())
+  }
 
-    pub(crate) fn app(&self) -> String {
-        self.app.clone().unwrap()
-    }
+  pub(crate) fn app(&self) -> String {
+    self.app.clone().unwrap()
+  }
 
-    pub(crate) fn is_none(&self) -> bool {
-        self.prefix.clone().is_none() && self.version.clone().is_none()
-    }
+  #[allow(dead_code)]
+  pub(crate) fn is_none(&self) -> bool {
+    self.prefix.clone().is_none() && self.version.clone().is_none()
+  }
+  #[allow(dead_code)]
+  pub(crate) fn is_some(&self) -> bool {
+    !self.is_none().clone()
+  }
 
-    pub(crate) fn is_some(&self) -> bool {
-        !self.is_none().clone()
-    }
-
-    pub(crate) fn set_app(&mut self, app: String) {
-        self.app = Some(app);
-    }
+  pub(crate) fn set_app(&mut self, app: String) {
+    self.app = Some(app);
+  }
 }
 
 pub(crate) struct ScriptResponse {
-    filename: String,
-    body: Cursor<Vec<u8>>,
-    body_size: usize,
+  filename: String,
+  body: Cursor<Vec<u8>>,
+  body_size: usize,
 }
 
 impl ScriptResponse {
-    pub(crate) fn new(filename: String, body: String) -> ScriptResponse {
-        let body = body.into_bytes();
-        let body_size = body.len();
-        let body = Cursor::new(body);
+  pub(crate) fn new(filename: String, body: String) -> ScriptResponse {
+    let body = body.into_bytes();
+    let body_size = body.len();
+    let body = Cursor::new(body);
 
-        ScriptResponse {
-            filename,
-            body,
-            body_size,
-        }
+    ScriptResponse {
+      filename,
+      body,
+      body_size,
     }
+  }
 }
 
 impl<'r> Responder<'r, 'static> for ScriptResponse {
-    fn respond_to(self, _req: &Request) -> response::Result<'static> {
-        let content_type = ContentType::new("application", "x-sh");
-        Response::build()
-            .status(Status::Ok)
-            .header(content_type)
-            .sized_body(self.body_size, self.body)
-            .header(http::Header::new(
-                CONTENT_DISPOSITION.as_str(),
-                format!("inline; filename=\"{}\"", self.filename),
-            ))
-            .ok()
-    }
+  fn respond_to(self, _req: &Request) -> response::Result<'static> {
+    let content_type = ContentType::new("application", "x-sh");
+    Response::build()
+      .status(Status::Ok)
+      .header(content_type)
+      .sized_body(self.body_size, self.body)
+      .header(http::Header::new(
+        CONTENT_DISPOSITION.as_str(),
+        format!("inline; filename=\"{}\"", self.filename),
+      ))
+      .ok()
+  }
 }
